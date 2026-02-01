dc0c65b276d8b20650fc1f31a7781f2b30b83404
CHESTERLTSDEV <lambychuchi176@gmail.com>
2026-02-01 23:48:38 +0200

    Add files via upload

---
+++ anon-sms/config.py
@@ -0,0 +1,4 @@
+API_TOKEN = '8390476876:AAHHN6aC4ZJOD6Iar1Jrh3Z82I2wJoZo03s' #TOKEN бота
+BOT_USERNAME = 'ltsstudiobot' # https://t.me/BOTUSERNAME Нужна для генерации реферальной ссылки
+ADMINS_ID = [6852461662, 676485072] # можно добавить много через запятую. Пример  [798661023, 798661023, 798661023]
+#DATABASE
