c9d1acaf52417b186c2eadd7fd54ae5e6ef32b0c
CHESTERLTSDEV <lambychuchi176@gmail.com>
2026-02-01 23:54:28 +0200

    Delete anon-sms directory

--- anon-sms/filters/filters.py
+++
@@ -1,8 +0,0 @@
-from aiogram.filters import BaseFilter
-
-class DeepLinkFilter(BaseFilter):
-    
-    async def __call__(self, message):
-        
-        return len(message.text.split()) > 1
-        
